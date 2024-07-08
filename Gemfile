4d7d5be0577abeecb4dd2ed02f3b64adc26a270a
Aneta <46348750+anetagloc@users.noreply.github.com>
2024-07-08 20:31:05 +0000

    changes

--- Gemfile
+++ Gemfile
@@ -46,8 +46,6 @@ gem "image_processing", "~> 1.2"
 
 gem "devise"
 
-gem 'rails-controller-testing'
-
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ]
--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -2,82 +2,80 @@ require 'rails_helper'
 
 RSpec.describe ArticlesController, type: :controller do
   describe "GET #index" do
-    subject(:index) { get :index }
+      subject(:index) { get :index }
 
-    it { is_expected.to be_successful }
+      it { is_expected.to be_successful }
   end
 
   describe "GET #show" do
-    subject(:show) { get :show, params: { id: article.id } }
-    let(:article) { create(:article) }
+      subject(:show) { get :show, params: { id: article.id } }
+      let(:article) { create(:article) }
 
-    it { is_expected.to be_successful }
+      it { is_expected.to be_successful }
   end
 
   describe "POST #create" do
-  context "when the article is valid" do
-    let(:valid_attributes) { { title: "Very valid title", body: "Very very valid value", published: true } } 
-  subject(:post_create) {post :create, params: { article: valid_attributes}}
-  it "creates new articles" do 
-    expect{subject}.to change(Article, :count).from(0).to(1)
+      context "when the article is valid" do
+      let(:valid_attributes) { { title: "Very valid title", body: "Very very valid value", published: true } } 
+      subject(:post_create) {post :create, params: { article: valid_attributes}}
+        it "creates new articles" do 
+          expect{subject}.to change(Article, :count).from(0).to(1)
  end
-it "redirect to article" do 
-    subject
-    expect(response).to redirect_to (articles_path)
+        it "redirect to article" do 
+          subject
+          expect(response).to redirect_to (articles_path)
 end
 end
-context "when the article is invalid" do
-  let(:invalid_attributes) { { title: "a", body: "This is a valid body." } } # Assuming the title is invalid
+      context "when the article is invalid" do
+        let(:invalid_attributes) { { title: "a", body: "This is a valid body." } } # Assuming the title is invalid
 
-  subject(:create_article) { post :create, params: { article: invalid_attributes } }
+        subject(:create_article) { post :create, params: { article: invalid_attributes } }
 
-  it "does not create a new article" do
-    expect { create_article }.not_to change(Article, :count)
+        it "does not create a new article" do
+        expect { create_article }.not_to change(Article, :count)
   end
 
-  it "renders the new template" do
-    create_article
-    expect(response).to have_http_status(:unprocessable_entity)  
-    expect(response).to render_template(:new) 
+        it "renders the new template" do
+          create_article
+          expect(response).to have_http_status(:unprocessable_entity)  
   end
 end
 end
 
 describe "PATCH #update" do
-let(:article) { create(:article) }
+  let(:article) { create(:article) }
 
-context "when title is valid" do
-  let(:new_title) { "Very good title" }
-  subject(:update) { patch :update, params: { id: article.id, article: { title: new_title } } }
+    context "when title is valid" do
+      let(:new_title) { "Very good title" }
+      subject(:update) { patch :update, params: { id: article.id, article: { title: new_title } } }
 
-  it "updates the article" do
-    subject
-    expect(article.reload.title).to eq(new_title)
+      it "updates the article" do
+        subject
+        expect(article.reload.title).to eq(new_title)
+      end
+
+      it "redirects to the article's show page" do
+        subject
+        expect(response).to redirect_to(article_path(article))
+      end
+    end
+
+    context "when title is invalid" do
+      let(:new_title) { "a" }  
+
+      subject(:update) { patch :update, params: { id: article.id, article: { title: new_title } } }
+
+      it "does not update the article" do
+        subject
+        expect(article.reload.title).not_to eq(new_title)
+      end
+
+      it "renders the edit template" do
+        subject
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
   end
-
-  it "redirects to the article's show page" do
-    subject
-    expect(response).to redirect_to(article_path(article))
-  end
-end
-
-context "when title is invalid" do
-  let(:new_title) { "a" }  
-
-  subject(:update) { patch :update, params: { id: article.id, article: { title: new_title } } }
-
-  it "does not update the article" do
-    subject
-    expect(article.reload.title).not_to eq(new_title)
-  end
-
-  it "renders the edit template" do
-    subject
-    expect(response).to have_http_status(:unprocessable_entity)
-    expect(response).to render_template(:edit)
-  end
-end
-end
 
   describe "DELETE #destroy" do
     subject(:destroy) { delete :destroy, params: { id: article.id } }
