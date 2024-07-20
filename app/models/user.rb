78f9a5cdf246adb6f0dbd1ab154f889cd4c348da
Aneta <46348750+anetagloc@users.noreply.github.com>
2024-07-20 13:59:10 +0000

    homework

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :validatable, :timeoutable
 
+  EMAIL_REGEX = Devise.email_regexp
+
   validates :username, presence: true
-  validates :email, presence: true
+  validates :email , presence: true,uniqueness:{case_sensetive:false}, format:{with:EMAIL_REGEX}
   validates :password, presence: true
+
+
+  # Permitted additional params for devise
+  def self.ransackable_attributes(auth_object = nil)
+    super + ['username']
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,19 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-    describe "validations" do
-        subject(:user) { user }
+  describe "validations" do
+    subject { user }
 
-        context "when username is empty" do
-            let(:user) { build(:user, username: nil) }
+    context "when username is empty" do
+      let(:user) { build(:user, username: nil) }
 
-            it "throws an error" do
-                expect(subject).to_not be_valid
-                expect(subject.errors[:username]).to include("can't be blank")
-            end
-        end
+      it "throws an error" do
+        expect(subject).to_not be_valid
+        expect(subject.errors[:username]).to include("can't be blank")
+      end
     end
+
+    context "when email is already taken" do
+      let!(:user1) { create(:user, email: "test@example.com") }
+      let(:user2) { build(:user, email: "test@example.com", password: "password", password_confirmation: "password") }
+
+      it 'does not allow creation of a user with an already taken email' do
+        expect(user2).not_to be_valid
+        expect(user2.errors[:email]).to include("has already been taken")
+      end
+    end
+
+    context "when email is invalid" do
+      let(:invalid_emails) { ["plainaddress"] }
+    
+      it 'does not allow creation of a user with an invalid email' do
+        invalid_emails.each do |invalid_email|
+          user = build(:user, email: invalid_email, password: "password", password_confirmation: "password")
+          expect(user).not_to be_valid
+    
+          unless user.valid?
+            puts "Testing invalid email: #{invalid_email}"
+            puts user.errors.full_messages
+          end
+    
+          expect(user.errors[:email]).to include("is invalid")
+        end
+      end
+    end
+  end
 end
-
-
-
